7df27309d2bcafade00e9fbb708ae5056f343764
iwai32 <iwakou32@gmail.com>
2020-03-29 19:46:08 +0900

    ページの外枠を作成

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -9,10 +9,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'author', content: 'KouheiIwai' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Play' },
+      { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+       integrity: 'sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN', crossorigin:  'anonymous'}
     ]
   },
   /*
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="container">
-   <p>top</p>
+  <div class="top-container">
+    <nuxt-link to='/admin/' class="to-admin-page">admin</nuxt-link>
+    <h1 class="key">Iwai's Portfolio</h1>
+
+    <ul class="top-nav">
+      <li>
+        <a href="profile/">Profile</a>
+      </li>
+      <li>
+        <a href="works/">Works</a>
+      </li>
+      <li>
+        <a href="https://github.com/iwai32?tab=repositories" target="_blank">Github</a>
+      </li>
+    </ul>
+    <p class="copy">
+      <small>&copy; 2019~ IwaiKouhei</small>
+    </p>
   </div>
 </template>
 
 <script>
-
-export default {
-}
+export default {};
 </script>
 
 <style>
